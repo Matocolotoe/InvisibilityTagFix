a2d984fcb22604e670808e70cc2a873b9fb4200c
Giovanni <42092549+Matocolotoe@users.noreply.github.com>
2021-08-20 19:43:26 +0200

    Store scoreboard teams directly

--- src/main/java/fr/giovanni75/tagfix/InvisibilityTagFix.java
+++ src/main/java/fr/giovanni75/tagfix/InvisibilityTagFix.java
@@ -33,10 +33,10 @@ public class InvisibilityTagFix extends JavaPlugin implements Listener {
 	private static Scoreboard scoreboard;
 
 	/**
-	 * A map used to store the name of the teams associated with players.
-	 * Avoids having to build the corresponding string on every task tick.
+	 * A map used to store the teams associated with players.
+	 * Avoids having to build the corresponding string every tick.
 	 */
-	private static final HashMap<Player, String> usernames = new HashMap<>();
+	private static final HashMap<Player, ScoreboardTeam> teams = new HashMap<>();
 
 	static CraftServer getCurrentServer() {
 		return server.server;
@@ -51,7 +51,7 @@ public class InvisibilityTagFix extends JavaPlugin implements Listener {
 	}
 
 	static ScoreboardTeam getTeam(Player p) {
-		return scoreboard.getTeam(usernames.get(p));
+		return teams.get(p);
 	}
 
 	/**
@@ -59,8 +59,9 @@ public class InvisibilityTagFix extends JavaPlugin implements Listener {
 	 * Called when a player quits the server or when the plugin gets disabled.
 	 */
 	private static void removePlayerTeam(Player p) {
-		final ScoreboardTeam team = scoreboard.getTeam(usernames.get(p));
-		if (team != null) scoreboard.removeTeam(team);
+		final ScoreboardTeam team = teams.get(p);
+		if (team != null)
+			scoreboard.removeTeam(team);
 	}
 
 	@Override
@@ -86,16 +87,19 @@ public class InvisibilityTagFix extends JavaPlugin implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onJoin(PlayerJoinEvent e) {
 		final String name = e.getPlayer().getUniqueId().toString().substring(0, 16);
-		if (scoreboard.getTeam(name) == null)
-			scoreboard.createTeam(name);
+
+		ScoreboardTeam team = scoreboard.getTeam(name);
+		if (team == null)
+			team = scoreboard.createTeam(name);
+
 		scoreboard.addPlayerToTeam(e.getPlayer().getName(), name);
-		usernames.put(e.getPlayer(), name);
+		teams.put(e.getPlayer(), team);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onQuit(PlayerQuitEvent e) {
 		removePlayerTeam(e.getPlayer());
-		usernames.remove(e.getPlayer());
+		teams.remove(e.getPlayer());
 	}
 
 }
